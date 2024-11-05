9f4014ce4e1fc2a39a748ac6008af80b1cece5bd
taniamikh <tatiana.mikhailova@mail.polimi.it>
2024-11-05 21:36:51 +0100

    Add files via upload

--- PBE_monodispersa.m
+++ PBE_monodispersa.m
@@ -6,7 +6,7 @@ clc
 
 %% Data
 
-global N kd
+global N
 
 kd = 1/200; %[1/s]
 N = 1000;
@@ -14,8 +14,8 @@ chain_length = 1:1:1000;
 
 %Monodisperse distribution
 
-P0 = zeros(1,999);
-P0 = [P0 1];
+P0 = zeros(1,1000);
+P0(1000) = 1;
 
 tetaspan = 0:1:500; %[s]
 
@@ -27,14 +27,14 @@ tetaspan = 0:1:500; %[s]
 
 figure(1)
 
-plot(chain_length, P0.*1e4, 'LineWidth',1.6, 'Color','black')
+plot(chain_length, P(50,:).*1e4, 'LineWidth',1.6, 'Color','black')
 hold on
 plot(chain_length, P(100,:).*1e4, 'LineWidth',1.6)
 plot(chain_length, P(250,:).*1e4, 'LineWidth',1.6)
 plot(chain_length, P(500,:).*1e4, 'LineWidth',1.6)
-axis([4 1000 0 100])
-legend('Teta = 0', 'Teta = 100','Teta = 250','Teta = 500')
-title('Log-normal Distribution')
+axis([5 1000 0 750])
+legend('Teta = 50', 'Teta = 100','Teta = 250','Teta = 500')
+title('Mono Dispersed Distribution')
 xlabel('Chain Length')
 ylabel('Normalized Concentration N*10^4')
 
@@ -50,11 +50,7 @@ dPdteta = zeros(N,1);
 
 %PBEs
 
-for i = 3 : N
-
-dPdteta(1) = sum(P(i)) + 2*P(2);
-
-end
+dPdteta(1) = sum(P(3:N)) + 2*P(2);
 
 for n = 2 : N-1
 
