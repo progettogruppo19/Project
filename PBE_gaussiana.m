8d9c8a4edad9c1fe378b56cc14ae60dd7db5c6da
taniamikh <tatiana.mikhailova@mail.polimi.it>
2024-11-06 17:51:42 +0100

    Add files via upload

--- PBE_gaussiana.m
+++ PBE_gaussiana.m
@@ -31,23 +31,59 @@ tspan = 0:10:1200000; %[s]
 teta = kd.*t;
 N = P;
 
+%Analytical solution
+
+k=50; 
+
+teta_1=0.01:50:6000;
+Pn_total=zeros(length(teta_1),5000+1);
+for t=1:length(teta_1)
+ Pn_final=zeros(1,5000+1);
+    for n=2:5000 
+ 
+    Pn_it=0;
+    for i=n:5000
+    Pn_old=Pn_it;
+        if i<n+k+1
+        Pn_in=P0(i)*(teta_1(t))^(i-n)/(factorial(i-n))*exp(-teta_1(t));
+        Pn_it=Pn_in+Pn_old;
+        else
+        Pn_in=P0(i)*1/(2*pi*teta_1(t))^0.5*exp(-(i-n-teta_1(t))^2/(2*teta_1(t)));
+        Pn_it=Pn_in+Pn_old;
+        end 
+    end
+ Pn_final(n)=Pn_it;
+end
+Pn_total(t,:)=Pn_final;
+end
+
+Pn_matrix=Pn_total(:,2:end);
+
 %% Plots
 
 figure(1)
 
 plot(chain_length(2), P0(2)*1e4, 'o', 'LineWidth',1.6)
 hold on
+
+
 title('Chain Length Distribution at teta = 0')
 xlabel('Chain Length')
 ylabel('Normalized Concentration N*10^4')
+
 plot(chain_length(50), P0(50)*1e4, 'o', 'LineWidth',1.6)
+
 plot(chain_length(100), P0(100)*1e4, 'o', 'LineWidth',1.6)
+
 plot(chain_length(500), P0(500)*1e4, 'o', 'LineWidth',1.6)
+
 plot(chain_length(1000), P0(1000)*1e4, 'o', 'LineWidth',1.6)
+
 plot(chain_length(2000), P0(2000)*1e4, 'o', 'LineWidth',1.6)
+
 hold on
 plot(chain_length, P0.*1e4, 'LineWidth',1.6, 'Color','black')
-legend('P0 2', 'P0 50','P0 100','P0 500','P0 1000','P0 2000')
+legend('P 2', 'P 50', 'P 100', 'P 500', 'P 1000', 'P 2000')
 hold off
 
 cc = jet(6);
@@ -56,15 +92,21 @@ figure(2)
 
 plot(teta,N(:,2).*1e4, 'LineWidth',1.6, 'Color',cc(1,:))
 hold on
+plot(teta_1,Pn_matrix(:,1).*1e4,"o",'Color',cc(1,:))
 plot(teta,N(:,50).*1e4, 'LineWidth',1.6, 'Color',cc(2,:))
+plot(teta_1,Pn_matrix(:,49).*1e4,"o",'Color',cc(2,:))
 plot(teta,N(:,100).*1e4, 'LineWidth',1.6, 'Color',cc(3,:))
+plot(teta_1,Pn_matrix(:,99).*1e4,"o",'Color',cc(3,:))
 plot(teta,N(:,500).*1e4, 'LineWidth',1.6, 'Color',cc(4,:))
+plot(teta_1,Pn_matrix(:,499).*1e4,"o",'Color',cc(4,:))
 plot(teta,N(:,1000).*1e4, 'LineWidth',1.6, 'Color',cc(5,:))
+plot(teta_1,Pn_matrix(:,999).*1e4,"o",'Color',cc(5,:))
 plot(teta,N(:,2000).*1e4, 'LineWidth',1.6, 'Color',cc(6,:))
+plot(teta_1,Pn_matrix(:,1999).*1e4,"o",'Color',cc(6,:))
 axis([0 6000 0 8])
 xlabel('Dimensionless Time (teta)')
 ylabel('Normalized Concentration N*10^4')
-legend('P 2', 'P 50', 'P 100', 'P 500', 'P 1000', 'P 2000')
+legend('P0 2 Exact', 'P0 2 Analytical', 'P0 50 Exact', 'P0 50 Analytical','P0 100 Exact', 'P0 100 Analytical','P0 500 Exact', 'P0 500 Analytical','P0 1000 Exact','P0 1000 Analytical','P0 2000 Exact', 'P0 2000 Analytical')
 
 %% Function
 
