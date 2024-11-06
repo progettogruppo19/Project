dcd0d63f4c1252233b7910eaa40ecb6886d8fb68
taniamikh <tatiana.mikhailova@mail.polimi.it>
2024-11-06 18:36:59 +0100

    Add files via upload

--- PBE_FS.m
+++ PBE_FS.m
@@ -26,17 +26,53 @@ tetaspan = 0:1:500; %[s]
 
 [teta,P] = ode15s(@PBE, tetaspan, P0, options);
 
+%Analytical solution
+
+k=50; 
+
+teta_1=0.01:1:500;
+Pn_total=zeros(length(teta_1),200+1);
+for t=1:length(teta_1)
+ Pn_final=zeros(1,200+1);
+    for n=2:200 
+ 
+    Pn_it=0;
+    for i=n:200
+    Pn_old=Pn_it;
+        if i<n+k+1
+        Pn_in=P0(i)*(teta_1(t))^(i-n)/(factorial(i-n))*exp(-teta_1(t));
+        Pn_it=Pn_in+Pn_old;
+        else
+        Pn_in=P0(i)*1/(2*pi*teta_1(t))^0.5*exp(-(i-n-teta_1(t))^2/(2*teta_1(t)));
+        Pn_it=Pn_in+Pn_old;
+        end 
+    end
+ Pn_final(n)=Pn_it;
+end
+Pn_total(t,:)=Pn_final;
+end
+
+Pn_matrix=Pn_total(:,2:end);
+
 %% Plots
+
+figure(1)
 
 figure(1)
 
 plot(chain_length, P0.*1e4, 'LineWidth',1.6, 'Color','black')
 hold on
-plot(chain_length, P(10,:).*1e4, 'LineWidth',1.6)
-plot(chain_length, P(25,:).*1e4, 'LineWidth',1.6)
-plot(chain_length, P(45,:).*1e4, 'LineWidth',1.6)
+plot(chain_length, P(10,:).*1e4, 'LineWidth',1.6, 'Color','blue')
+plot(chain_length,Pn_matrix(10,:).*1e4,"o",  'Color','blue')
+
+plot(chain_length, P(25,:).*1e4, 'LineWidth',1.6, 'Color','green')
+plot(chain_length,Pn_matrix(25,:).*1e4,"o", 'Color', 'green')
+
+plot(chain_length, P(45,:).*1e4, 'LineWidth',1.6, 'Color','magenta')
+plot(chain_length,Pn_matrix(45,:).*1e4,"o", 'Color','magenta')
+
 axis([2.3 100 0 200])
-legend('Teta = 0', 'Teta = 100','Teta = 250','Teta = 500')
+legend('Teta = 0', 'Teta = 100 - Exact Solution','Teta = 100 - Analytical Solution','Teta = 250 - Exact Solution','Teta = 250 - Analytical Solution','Teta = 500 - Exact Solution', 'Teta = 500 - Analytical Solution')
 title('Flory Distribution')
 xlabel('Chain Length')
 ylabel('Normalized Concentration N*10^4')
