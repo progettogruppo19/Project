f156ba552f3adf1e1c28b47247008f52c5782a54
taniamikh <tatiana.mikhailova@mail.polimi.it>
2024-11-06 17:51:03 +0100

    Add files via upload

--- MOM.m
+++ MOM.m
@@ -6,7 +6,7 @@ clc
 
 %% Data 
 
-global P0 maxCL x Pn_matrix teta_span
+global P0 maxCL x teta_span
 
 kd = 1/200; 
 D = 1.5;
@@ -38,7 +38,7 @@ lambda2_in = sum(x.^2.*P0);
 
 initialcond =[lambda0_in lambda1_in lambda2_in 0];
 
-teta_span = [0 600];
+teta_span = [0.01 600];
 
 %ODE
 
@@ -51,28 +51,45 @@ lambda1 = y(:,2);
 lambda2 = y(:,3);
 M = y(:,4);
 
+[teta_1,y_1] = ode15s(@MOM_fun_no_P2,teta_span,initialcond, options);
+
+t_ad_1 = teta_1';
+lambda0_1 = y_1(:,1);
+lambda1_1 = y_1(:,2);
+lambda2_1 = y_1(:,3);
+M_1 = y(:,4);
+
 %% Plots 
 
 figure(1)
 plot(t_ad,lambda0',LineWidth=1.5)
+hold on
+plot(t_ad_1,lambda0_1',LineWidth=1.5)
 title('Lambda 0')
 xlabel('Dimensionless time (Teta)')
 ylabel('Lambda 0')
-xlim([0 600])
+axis([0 600 0 1.2])
+legend('Solution considering P2', 'Solution without considering P2')
 
 figure(2)
 plot(t_ad,lambda1',LineWidth=1.5)
+hold on
+plot(t_ad_1,lambda1_1',"o",LineWidth=1.5)
 title('Lambda 1')
 xlabel('Dimensionless time (Teta)')
 ylabel('Lambda 1')
 xlim([0 600])
+legend('Solution considering P2', 'Solution without considering P2')
 
 figure(3)
 plot(t_ad,lambda2',LineWidth=1.5)
+hold on
+plot(t_ad_1,lambda2_1',"o",LineWidth=1.5)
 title('Lambda 2')
 xlabel('Dimensionless time (Teta)')
 ylabel('Lambda 2')
 xlim([0 600])
+legend('Solution considering P2', 'Solution without considering P2')
 
 figure(4)
 plot(t_ad,M',LineWidth=1.5)
@@ -85,7 +102,7 @@ xlim([0 600])
 
 function mom = MOM_fun(teta,y)
 
-global P0 maxCL x teta_span Pn_matrix
+global P0 maxCL x teta_span
 
 lambda0 = y(1);
 lambda1 = y(2);
@@ -143,6 +160,69 @@ dl0dteta = -P2;
 dl1dteta = -lambda0-P2; 
 dl2dteta = -2*lambda1+lambda0-P2; 
 dMdteta = lambda0+P2; 
+
+mom = [dl0dteta dl1dteta dl2dteta dMdteta]';
+
+end
+
+function mom = MOM_fun_no_P2(teta,y)
+
+global P0 maxCL x teta_span
+
+lambda0 = y(1);
+lambda1 = y(2);
+lambda2 = y(3);
+M = y(4);
+
+k = 50;
+
+Pn_final = zeros(1,maxCL+1);
+
+for n = 2:maxCL 
+
+    Pn_it = 0;  
+
+    for ii = n:maxCL
+
+        if ii <= k + n
+
+            Pn_in = P0(ii) * (teta)^(ii - n) / factorial(ii - n) * exp(-teta);
+
+        else
+
+            Pn_in = P0(ii) / sqrt(2 * pi * teta) * exp(-(ii - n - teta)^2 / (2 * teta));
+
+        end
+
+        Pn_it = Pn_it + Pn_in;  
+    end
+
+    Pn_final(n) = Pn_it;  
+
+end
+
+Pn = Pn_final(2:end);
+
+Pn_matrix = zeros(length(teta_span),length(Pn));
+
+for i=1:length(teta)
+
+ Pn_matrix(i,:)=Pn;
+
+end
+
+%Lambdas
+
+lambda0 = sum(Pn);
+lambda1 = sum(x.*Pn);
+lambda2 = sum(x.^2.*Pn);
+
+%Equations 
+
+dl0dteta = 0;
+dl1dteta = -lambda0; 
+dl2dteta = -2*lambda1+lambda0; 
+dMdteta = lambda0; 
 
 mom = [dl0dteta dl1dteta dl2dteta dMdteta]';
 
