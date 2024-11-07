a3cd834d6ed827d5017131c226395e67d6d8c216
taniamikh <tatiana.mikhailova@mail.polimi.it>
2024-11-07 10:25:24 +0100

    Add files via upload

--- MOM.m
+++ MOM.m
@@ -51,6 +51,11 @@ lambda1 = y(:,2);
 lambda2 = y(:,3);
 M = y(:,4);
 
+yield_1 = 1 - lambda1./lambda1_in;
+xn_1 = lambda1./lambda0;
+xm_1 = lambda2./lambda1;
+D_1 = xm_1./xn_1;
+
 [teta_1,y_1] = ode15s(@MOM_fun_no_P2,teta_span,initialcond, options);
 
 t_ad_1 = teta_1';
@@ -59,44 +64,73 @@ lambda1_1 = y_1(:,2);
 lambda2_1 = y_1(:,3);
 M_1 = y(:,4);
 
+yield_2 = 1 - lambda1_1/lambda1_in;
+xn_2 = lambda1_1./lambda0_1;
+xm_2 = lambda2_1./lambda1_1;
+D_2 = xm_2./xn_2;
+
 %% Plots 
 
 figure(1)
-plot(t_ad,lambda0',LineWidth=1.5)
+plot(t_ad,lambda0',LineWidth=2)
 hold on
-plot(t_ad_1,lambda0_1',LineWidth=1.5)
+plot(t_ad_1,lambda0_1',LineWidth=2)
 title('Lambda 0')
 xlabel('Dimensionless time (Teta)')
 ylabel('Lambda 0')
-axis([0 600 0 1.2])
+axis([0 500 0 1.2])
 legend('Solution considering P2', 'Solution without considering P2')
 
 figure(2)
-plot(t_ad,lambda1',LineWidth=1.5)
+plot(t_ad,lambda1',LineWidth=2)
 hold on
-plot(t_ad_1,lambda1_1',"o",LineWidth=1.5)
+plot(t_ad_1,lambda1_1',"o",LineWidth=2)
 title('Lambda 1')
 xlabel('Dimensionless time (Teta)')
 ylabel('Lambda 1')
-xlim([0 600])
+xlim([0 500])
 legend('Solution considering P2', 'Solution without considering P2')
 
 figure(3)
-plot(t_ad,lambda2',LineWidth=1.5)
+plot(t_ad,lambda2',LineWidth=2)
 hold on
-plot(t_ad_1,lambda2_1',"o",LineWidth=1.5)
+plot(t_ad_1,lambda2_1',"o",LineWidth=2)
 title('Lambda 2')
 xlabel('Dimensionless time (Teta)')
 ylabel('Lambda 2')
-xlim([0 600])
+xlim([0 500])
 legend('Solution considering P2', 'Solution without considering P2')
 
 figure(4)
-plot(t_ad,M',LineWidth=1.5)
+plot(t_ad,M',LineWidth=2)
 xlabel('Dimensionless time (Teta)')
 ylabel('Monomer concentration')
 title('M')
-xlim([0 600])
+xlim([0 500])
+
+figure(5)
+plot(t_ad, yield_1, LineWidth=2)
+title('Yield with respect to Teta')
+xlabel('Dimensionless time (Teta)')
+ylabel('Yield')
+axis([0 500 0 0.5])
+
+figure(6)
+plot(t_ad, xn_1, LineWidth=2)
+hold on
+plot(t_ad, xm_1, LineWidth=2)
+title('Average Chain Length with respect to Teta')
+xlabel('Dimensionless time (Teta)')
+ylabel('Average Chain Length')
+legend('Xn', 'Xm')
+axis([0 500 600 1800])
+
+figure(7)
+plot(t_ad, D_1, LineWidth=2)
+title('Dispersity with respect to Teta')
+xlabel('Dimensionless time (Teta)')
+ylabel('Dispersity')
+axis([0 500 1.4 1.9])
 
 %% Functions 
 
